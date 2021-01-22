4228ecf529848154d30117e7f6906e39e6e47af0
Alex <alexfrancisaaron@gmail.com>
2021-01-22 13:12:06 -0600

    commit tree 282f47233990feec4021f0a3e93750607890b2df

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Alex Aaron"
+    end
+
+    get '/hometown' do 
+        "My hometown is Baton Rouge, Louisiana"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'Shady Lane' by Pavement"
+    end
+
 end
